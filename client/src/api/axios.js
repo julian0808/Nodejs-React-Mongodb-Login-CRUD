8f9bab6d4a951153f78df9c1bb165e75b84d6b0b
julian0808 <41093464+julian0808@users.noreply.github.com>
2025-05-16 15:13:03 -0500

    Third app commit

--- client/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:4000/api", // Use http for local dev unless HTTPS is configured
-  withCredentials: true, // Needed for cookies/session handling
-});
-
-export default instance;
